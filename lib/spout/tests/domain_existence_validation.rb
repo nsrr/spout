3288fa1bac79c52d92e99195cbacc603a0579905
Remo Mueller <remosm@gmail.com>
2013-05-16 13:52:39 -0400

    Improved the domain_existence assert error messages

--- lib/spout/tests/domain_existence_validation.rb
+++ lib/spout/tests/domain_existence_validation.rb
@@ -2,10 +2,24 @@ module Spout
   module Tests
     module DomainExistenceValidation
 
+      def assert_domain_existence(item, msg = nil)
+        result = begin
+          domain_name = JSON.parse(File.read(item))["domain"]+".json"
+          File.exists?(File.join("domains", domain_name))
+        rescue JSON::ParserError => e
+          error = e
+          false
+        end
+        full_message = build_message(msg, "The domain \"domains/#{domain_name}\" referenced by ? does not exist.", item)
+        assert_block(full_message) do
+          result
+        end
+      end
+
       Dir.glob("variables/**/*.json").each do |file|
         if (not [nil, ''].include?(JSON.parse(File.read(file))["domain"]) rescue false)
           define_method("test_domain_exists: "+file) do
-            assert_equal true, (File.exists?(File.join("domains", JSON.parse(File.read(file))["domain"]+".json")) rescue false)
+            assert_domain_existence file
           end
         end
       end
