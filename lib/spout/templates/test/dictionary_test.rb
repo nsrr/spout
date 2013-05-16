89097ff02018f78447ec3e00eef6bb2c5752e84a
Remo Mueller <remosm@gmail.com>
2013-05-16 13:17:00 -0400

    Use standard ruby method for defining tests

--- lib/spout/templates/test/dictionary_test.rb
+++ lib/spout/templates/test/dictionary_test.rb
@@ -4,7 +4,7 @@ class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
 
   # You may add additional tests here
-  # test "the truth" do
+  # def test_truth
   #   assert true
   # end
 end
