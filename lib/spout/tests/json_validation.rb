c5856b5dcb9aaca6b52c15e6d1d9064517eaf5d2
Remo Mueller <remosm@gmail.com>
2013-05-16 13:38:56 -0400

    Add JSON file assertion for better invalid json assertion error output

--- lib/spout/tests/json_validation.rb
+++ lib/spout/tests/json_validation.rb
@@ -2,15 +2,28 @@ module Spout
   module Tests
     module JsonValidation
 
+      def assert_valid_json(item, msg = nil)
+        result = begin
+          !!JSON.parse(File.read(item))
+        rescue JSON::ParserError => e
+          error = e
+          false
+        end
+        full_message = build_message(msg, "?", error)
+        assert_block(full_message) do
+          result
+        end
+      end
+
       Dir.glob("variables/**/*.json").each do |file|
         define_method("test_json: "+file) do
-          assert_equal true, (!!JSON.parse(File.read(file)) rescue false)
+          assert_valid_json file
         end
       end
 
       Dir.glob("domains/**/*.json").each do |file|
         define_method("test_json: "+file) do
-          assert_equal true, (!!JSON.parse(File.read(file)) rescue false)
+          assert_valid_json file
         end
       end
 
