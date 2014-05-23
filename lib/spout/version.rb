9c82cca50a1671ddbe8393e8b8e69c8ec951c930
Remo Mueller <remosm@gmail.com>
2014-05-23 12:12:56 -0400

    Version bump to 0.8.0.beta1

--- lib/spout/version.rb
+++ lib/spout/version.rb
@@ -3,7 +3,7 @@ module Spout
     MAJOR = 0
     MINOR = 8
     TINY = 0
-    BUILD = "pre" # nil, "pre", "rc", "rc2"
+    BUILD = "beta1" # nil, "pre", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
