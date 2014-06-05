cc052c2585363880d891309c14af1cedeffcf20f
Remo Mueller <remosm@gmail.com>
2014-06-05 09:48:34 -0400

    Exporter test updates

--- test/application/exporter_test.rb
+++ test/application/exporter_test.rb
@@ -17,21 +17,15 @@ module ApplicationTests
     end
 
     def test_exports
-      variable_csv = <<-CSV
-folder,id,display_name,description,type,units,domain,labels,calculation
-"",age_at_visit,Age at Visit,Age at time of visit.,numeric,years,"",age_at_visit,""
-"",gender,Gender,Gender as reported by Parent Cohort,choices,"",gdomain,gender,""
-      CSV
-
-      domain_csv = <<-CSV
-folder,domain_id,value,display_name,description
-"",gdomain,m,Male,""
-"",gdomain,f,Female,""
-      CSV
+      variable_csv = "folder,id,display_name,description,type,units,domain,labels,calculation\n\"\",age_at_visit,Age at Visit,Age at time of visit.,numeric,years,\"\",age_at_visit,\"\"\n\"\",gender,Gender,Gender as reported by Parent Cohort,choices,\"\",gdomain,gender,\"\"\n"
+      domain_csv = "folder,domain_id,value,display_name,description\n\"\",gdomain,m,Male,\"\"\n\"\",gdomain,f,Female,\"\"\n"
 
       output, error = util_capture do
         Dir.chdir(app_path) { Spout.launch ['export'] }
       end
+
+      puts File.read(File.join(app_path, 'dd', '1.0.0', 'variables.csv'))
+      puts File.read(File.join(app_path, 'dd', '1.0.0', 'domains.csv'))
 
       assert File.directory?(File.join(app_path, 'dd'))
       assert_equal ["age_at_visit", "gender"].sort, Dir.glob(File.join(app_path, 'variables', '**', '*.json')).collect{|s| s.gsub(/^(.*)\/|\.json$/, '')}.sort
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 
 require 'minitest/autorun'
-# require 'minitest/reporters'
-# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require File.expand_path('../../lib/spout', __FILE__)
