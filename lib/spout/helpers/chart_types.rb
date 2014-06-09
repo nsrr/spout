c299f4c80c6b7138a8751b4d34153dcb7cd7b799
Remo Mueller <remosm@gmail.com>
2014-06-09 15:11:31 -0400

    Fixed an issue where computing precision returned -Infinity due to passing a zero to a logarithmic function. Added test cases for computing continuous buckets and value bucket placement

--- lib/spout/helpers/chart_types.rb
+++ lib/spout/helpers/chart_types.rb
@@ -5,11 +5,15 @@ module Spout
   module Helpers
     class ChartTypes
       def self.get_bucket(buckets, value)
-        return nil if buckets.size == 0
+        return nil if buckets.size == 0 or not value.kind_of?(Numeric)
         buckets.each do |b|
           return "#{b[0]} to #{b[1]}" if value >= b[0] and value <= b[1]
         end
-        "#{buckets.last[0]} to #{buckets.last[1]}"
+        if value <= buckets.first[0]
+          "#{buckets.first[0]} to #{buckets.first[1]}"
+        else
+          "#{buckets.last[0]} to #{buckets.last[1]}"
+        end
       end
 
       def self.continuous_buckets(values)
@@ -19,7 +23,7 @@ module Spout
         maximum_bucket = values.max
         max_buckets = 12
         bucket_size = ((maximum_bucket - minimum_bucket) / max_buckets.to_f)
-        precision = [-Math.log10(bucket_size).floor, 0].max
+        precision = (bucket_size == 0 ? 0 : [-Math.log10(bucket_size).floor, 0].max)
 
         buckets = []
         (0..(max_buckets-1)).to_a.each do |index|
---
+++ test/helpers/chart_types_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+require 'spout/helpers/chart_types'
+
+module HelperTests
+  class ChartTypesTest < Minitest::Test
+
+    def test_precision_for_infinity
+      assert_equal [[0, 0]]*12, Spout::Helpers::ChartTypes::continuous_buckets([0,0])
+    end
+
+    def test_buckets_with_zero_precision
+      assert_equal [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [7, 8], [8, 9], [9, 10], [10, 11], [11, 12], [12, 13]], Spout::Helpers::ChartTypes::continuous_buckets([0.0,13.0])
+    end
+
+    def test_buckets_with_one_precision
+      assert_equal [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5], [0.5, 0.6], [0.6, 0.7], [0.7, 0.8], [0.8, 0.9], [0.9, 1.0], [1.0, 1.1], [1.1, 1.2], [1.2, 1.3]], Spout::Helpers::ChartTypes::continuous_buckets([0.1,1.3])
+    end
+
+    def test_buckets_with_two_precision
+      assert_equal [[0.01, 0.02], [0.02, 0.03], [0.03, 0.04], [0.04, 0.05], [0.05, 0.06], [0.06, 0.07], [0.07, 0.08], [0.08, 0.09], [0.09, 0.1], [0.1, 0.11], [0.11, 0.12], [0.12, 0.13]], Spout::Helpers::ChartTypes::continuous_buckets([0.01,0.13])
+    end
+
+    def test_get_bucket_with_empty_buckets
+      assert_equal nil, Spout::Helpers::ChartTypes::get_bucket([], 1)
+    end
+
+    def test_get_bucket_labels
+      buckets = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5], [0.5, 0.6], [0.6, 0.7], [0.7, 0.8], [0.8, 0.9], [0.9, 1.0], [1.0, 1.1], [1.1, 1.2], [1.2, 1.3]]
+      assert_equal "0.1 to 0.2", Spout::Helpers::ChartTypes::get_bucket(buckets, 0.1)
+      assert_equal "0.1 to 0.2", Spout::Helpers::ChartTypes::get_bucket(buckets, 0.2)
+      assert_equal "0.2 to 0.3", Spout::Helpers::ChartTypes::get_bucket(buckets, 0.21)
+      assert_equal "0.2 to 0.3", Spout::Helpers::ChartTypes::get_bucket(buckets, 0.3)
+    end
+
+    # Underflow gets placed into the first bucket
+    # The first and the last bucket may be relabeled to specify this
+    def test_get_bucket_underflow
+      buckets = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5], [0.5, 0.6], [0.6, 0.7], [0.7, 0.8], [0.8, 0.9], [0.9, 1.0], [1.0, 1.1], [1.1, 1.2], [1.2, 1.3]]
+      assert_equal "0.1 to 0.2", Spout::Helpers::ChartTypes::get_bucket(buckets, 0.0)
+    end
+
+    # Overflow gets placed into the last bucket
+    # The first and the last bucket may be relabeled to specify this
+    def test_get_bucket_overflow
+      buckets = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5], [0.5, 0.6], [0.6, 0.7], [0.7, 0.8], [0.8, 0.9], [0.9, 1.0], [1.0, 1.1], [1.1, 1.2], [1.2, 1.3]]
+      assert_equal "1.2 to 1.3", Spout::Helpers::ChartTypes::get_bucket(buckets, 1.4)
+    end
+
+    def test_get_bucket_for_nil_or_non_number_value
+      buckets = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5], [0.5, 0.6], [0.6, 0.7], [0.7, 0.8], [0.8, 0.9], [0.9, 1.0], [1.0, 1.1], [1.1, 1.2], [1.2, 1.3]]
+      assert_equal nil, Spout::Helpers::ChartTypes::get_bucket(buckets, nil)
+      assert_equal nil, Spout::Helpers::ChartTypes::get_bucket(buckets, "m")
+      assert_equal nil, Spout::Helpers::ChartTypes::get_bucket(buckets, "1")
+    end
+
+  end
+end
