d37c84853cd05d51491a632e40a7a57952129dec
Remo Mueller <remosm@gmail.com>
2015-10-15 16:11:12 -0400

    Version bump to 0.11.0.pre

--- lib/spout/version.rb
+++ lib/spout/version.rb
@@ -1,9 +1,9 @@
 module Spout
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 10
-    TINY = 2
-    BUILD = nil # nil, "pre", "rc", "rc2"
+    MINOR = 11
+    TINY = 0
+    BUILD = 'pre' # 'pre', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
