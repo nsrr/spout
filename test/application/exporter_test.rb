381b1b3f86b24153ae5900f30420da3a51d09ede
Remo Mueller <remosm@gmail.com>
2017-03-24 16:04:08 -0400

    Code cleanup

--- test/application/exporter_test.rb
+++ test/application/exporter_test.rb
@@ -4,8 +4,8 @@ require 'test_helpers/sandbox'
 require 'test_helpers/capture'
 
 module ApplicationTests
+  # Tests to assure that dictionary exports work.
   class ExporterTest < SandboxTest
-
     include TestHelpers::Capture
 
     def setup
@@ -24,23 +24,20 @@ folder,id,display_name,description,type,units,domain,labels,calculation,commonly
 "",age_at_visit,Age at Visit,Age at time of visit.,numeric,years,"",age_at_visit,"",true,""
 "",gender,Gender,Gender as reported by Parent Cohort,choices,"",gdomain,gender,"",true,intake_questionnaire
       CSV
-
       domain_csv = <<-CSV
 folder,domain_id,value,display_name,description
 "",gdomain,m,Male,""
 "",gdomain,f,Female,""
 "",gdomain,r,Refused to Answer,""
       CSV
-
       output, _error = util_capture do
         Dir.chdir(app_path) { Spout.launch ['export'] }
       end
-
       assert File.directory?(File.join(app_path, 'dd'))
       assert_equal variable_csv, File.read(File.join(app_path, 'dd', '1.0.0', 'variables.csv'))
       assert_equal domain_csv, File.read(File.join(app_path, 'dd', '1.0.0', 'domains.csv'))
-      assert_match "dd/1.0.0/variables.csv", output
-      assert_match "dd/1.0.0/domains.csv", output
+      assert_match 'dd/1.0.0/variables.csv', output
+      assert_match 'dd/1.0.0/domains.csv', output
     end
 
     def test_exports_with_slug_specified
@@ -53,29 +50,27 @@ charts:
   - chart: gender
     title: Gender
       YML
-
       variable_csv = <<-CSV
 folder,id,display_name,description,type,units,domain,labels,calculation,commonly_used,forms
 "",age_at_visit,Age at Visit,Age at time of visit.,numeric,years,"",age_at_visit,"",true,""
 "",gender,Gender,Gender as reported by Parent Cohort,choices,"",gdomain,gender,"",true,intake_questionnaire
       CSV
-
       domain_csv = <<-CSV
 folder,domain_id,value,display_name,description
 "",gdomain,m,Male,""
 "",gdomain,f,Female,""
 "",gdomain,r,Refused to Answer,""
       CSV
-
       output, _error = util_capture do
         Dir.chdir(app_path) { Spout.launch ['export'] }
       end
-
       assert File.directory?(File.join(app_path, 'dd'))
-      assert_equal variable_csv, File.read(File.join(app_path, 'dd', '1.0.0', 'myrepo-data-dictionary-1.0.0-variables.csv'))
+      assert_equal variable_csv, File.read(
+        File.join(app_path, 'dd', '1.0.0', 'myrepo-data-dictionary-1.0.0-variables.csv')
+      )
       assert_equal domain_csv, File.read(File.join(app_path, 'dd', '1.0.0', 'myrepo-data-dictionary-1.0.0-domains.csv'))
-      assert_match "dd/1.0.0/myrepo-data-dictionary-1.0.0-variables.csv", output
-      assert_match "dd/1.0.0/myrepo-data-dictionary-1.0.0-domains.csv", output
+      assert_match 'dd/1.0.0/myrepo-data-dictionary-1.0.0-variables.csv', output
+      assert_match 'dd/1.0.0/myrepo-data-dictionary-1.0.0-domains.csv', output
     end
 
     def test_export_creates_forms_csv
@@ -90,7 +85,7 @@ folder,id,display_name,code_book
 
       assert File.directory?(File.join(app_path, 'dd'))
       assert_equal form_csv, File.read(File.join(app_path, 'dd', '1.0.0', 'forms.csv'))
-      assert_match "dd/1.0.0/forms.csv", output
+      assert_match 'dd/1.0.0/forms.csv', output
     end
   end
 end
