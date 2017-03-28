300bd77a17390640ca7941fc437c1f6c78bd6b2d
Remo Mueller <remosm@gmail.com>
2017-03-28 11:34:42 -0400

    Version bump to 0.12.0.rc2

--- lib/spout/version.rb
+++ lib/spout/version.rb
@@ -5,7 +5,7 @@ module Spout
     MAJOR = 0
     MINOR = 12
     TINY = 0
-    BUILD = 'rc' # 'pre', 'rc', 'rc2', nil
+    BUILD = 'rc2' # 'pre', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
