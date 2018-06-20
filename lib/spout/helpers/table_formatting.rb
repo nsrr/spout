b1c3a21d1d99e431bc9e80370baf675237ba927b
Remo Mueller <remosm@gmail.com>
2018-06-20 14:22:10 -0400

    Delimiter is now correctly used in method

--- lib/spout/helpers/table_formatting.rb
+++ lib/spout/helpers/table_formatting.rb
@@ -8,7 +8,7 @@ module Spout
       # end
 
       def self.number_with_delimiter(number, delimiter = ",")
-        number.to_s.reverse.scan(/(?:\d*\.)?\d{1,3}-?/).join(",").reverse
+        number.to_s.reverse.scan(/(?:\d*\.)?\d{1,3}-?/).join(delimiter).reverse
       end
 
       # type:  :count    or   :decimal
